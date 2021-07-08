8492eb29d4b8b7d1a852866cc405b59c0a64e21a
Abhisindh Chatterjee <abhisindh123@gmail.com>
2021-07-08 08:21:52 +0530

    emaijs function updated

--- js/mail.js
+++ js/mail.js
@@ -1,11 +1,17 @@
-function sendMail(params){
-    var tempParams={
-        user_name:document.getElementById("user_name").value,
-        user_email:document.getElementById("user_email").value,
-        message:document.getElementById("message").value
-};
-    emailjs.send('service_f77fmp2','contact_form',tempParams)
-    .then(function(res){
-        console.log("success",res.status);
-    })
+(function() {
+    emailjs.init("user_5EbI5orzDfnG1kbTlteYs");
+    })();
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_f77fmp2', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
 }
