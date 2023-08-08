3dd9232ad3540905a3cd24f74aa6e2b73543983e
Abhisindh Chatterjee <abhisindh123@gmail.com>
2023-08-08 09:02:36 +0530

    small changes

--- assets/js/blog_data.js
+++ assets/js/blog_data.js
@@ -1,12 +1,5 @@
 export const blogList = [
-    {
-        "id": "001",
-        "header_img": "../blogs/images/001-header.jpeg",
-        "title": "Mastering Dynamic Arrays in C: Building Adaptable Data Structures",
-        "author": "Abhisindh",
-        "description": " Imagine you are a librarian in a busy library, tasked with managing an ever growing collection of books.You start with a set number of bookshelves neatly organized with a fixed number of slots of books.However as more and more book-enthusiasts visit your library, the shelves fill up, and you are faced with a dilemma- what if you run out of space for new books ?",
-        "path": "../../blogs/001.html"
-    },
+    
     {
         "id": "002",
         "header_img": "../blogs/images/002-header.jpg",
@@ -16,7 +9,15 @@ export const blogList = [
         "path": "../../blogs/002.html"
     },
     {
-        "id": "003",
+        "id": "001",
+        "header_img": "../blogs/images/001-header.jpeg",
+        "title": "Mastering Dynamic Arrays in C: Building Adaptable Data Structures",
+        "author": "Abhisindh",
+        "description": " Imagine you are a librarian in a busy library, tasked with managing an ever growing collection of books.You start with a set number of bookshelves neatly organized with a fixed number of slots of books.However as more and more book-enthusiasts visit your library, the shelves fill up, and you are faced with a dilemma- what if you run out of space for new books ?",
+        "path": "../../blogs/001.html"
+    },
+    {
+        "id": "000",
         "header_img": "",
         "title": "Title 3",
         "author": "Author 3",
