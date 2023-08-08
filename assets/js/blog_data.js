21a3c66d34e943f6081bae7afde71ed99d2cf2d8
Abhisindh Chatterjee <abhisindh123@gmail.com>
2023-08-08 09:15:30 +0530

    image link error

--- assets/js/blog_data.js
+++ assets/js/blog_data.js
@@ -1,8 +1,17 @@
+// {
+//     "id": "000",
+//     "header_img": "",
+//     "title": "Title 3",
+//     "author": "Author 3",
+//     "description": "Description for blog 3.",
+//     "path": "../../blogs/003.html"
+// }
+
 export const blogList = [
     
     {
         "id": "002",
-        "header_img": "../blogs/images/002-header.jpg",
+        "header_img": "../../blogs/images/002-header.jpg",
         "title": "Exploring Tree Data Structures: Binary Trees and Binary Search Trees",
         "author": "Abhisindh",
         "description": "Tree data structures play a crucial role in computer science, providing efficient storage and retrieval of data in hierarchical formats. In this article, we'll delve into two essential tree structures: Binary Trees and Binary Search Trees (BSTs). Understanding these structures will enable you to solve a wide range of problems efficiently.",
@@ -10,18 +19,12 @@ export const blogList = [
     },
     {
         "id": "001",
-        "header_img": "../blogs/images/001-header.jpeg",
+        "header_img": "../../blogs/images/001-header.jpeg",
         "title": "Mastering Dynamic Arrays in C: Building Adaptable Data Structures",
         "author": "Abhisindh",
         "description": " Imagine you are a librarian in a busy library, tasked with managing an ever growing collection of books.You start with a set number of bookshelves neatly organized with a fixed number of slots of books.However as more and more book-enthusiasts visit your library, the shelves fill up, and you are faced with a dilemma- what if you run out of space for new books ?",
         "path": "../../blogs/001.html"
     },
-    {
-        "id": "000",
-        "header_img": "",
-        "title": "Title 3",
-        "author": "Author 3",
-        "description": "Description for blog 3.",
-        "path": "../../blogs/003.html"
-    }
+
 ]
+
